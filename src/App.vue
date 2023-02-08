5a8f07099d13557038fa91007904a15c90a40ad9
Christoph Haid <christoph@MBPvonChristoph.fritz.box>
2023-02-08 13:49:59 +0100

    emptyProject

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,145 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
+import { ref } from "vue";
+
+//New Todo
+const newTodo = ref("");
+const defaultData = [
+  {
+    done: false,
+    content: "Einkaufen gehen",
+  },
+];
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  <h1>ToDo App</h1>
+  <form>
+    <label>Neue Aufgabe hinzufügen: </label>
+    <input
+      placehodler="Enter your Todo here"
+      name="newTodo"
+      autocomplete="off"
+    />
+  </form>
+  <h2>Deine Aufgaben:</h2>
+  <ul>
+    <li>
+      <span>Todo</span>
+      <button>Entfernen</button>
+    </li>
+  </ul>
+  <h4>Deine Aufgabenliste ist leer 🤟</h4>
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
+<style lang="scss">
+:root {
+  --border: 2px solid rgba(255, 255, 255, 0.35);
+  --size1: 6px;
+  --size2: 12px;
+  --size3: 18px;
+  --size4: 24px;
+  --size5: 48px;
+  --backgroundColor: #27292d;
+  --textColor: white;
+  --primaryColor: #a0a4d9;
+  --secondTextColor: #1f2023;
 }
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
+
+body {
+  margin: 0;
+  padding: 0;
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  background-color: var(--backgroundColor);
+  color: var(--textColor);
+  #app {
+    max-width: 600px;
+    margin-left: auto;
+    margin-right: auto;
+    padding: 20px;
+    h1 {
+      font-weight: bold;
+      font-size: 28px;
+      text-align: center;
+    }
+    form {
+      display: flex;
+      flex-direction: column;
+      width: 100%;
+      label {
+        font-size: 14px;
+        font-weight: bold;
+      }
+      input,
+      button {
+        height: var(--size5);
+        box-shadow: none;
+        outline: none;
+        padding-left: var(--size2);
+        padding-right: var(--size2);
+        border-radius: var(--size1);
+        font-size: 18px;
+        margin-top: var(--size1);
+        margin-bottom: var(--size2);
+      }
+      button:disabled,
+      button[disabled] {
+        border: 1px solid #999999;
+        background-color: #cccccc;
+        color: #666666;
+        cursor: not-allowed;
+      }
+      input {
+        background-color: transparent;
+        border: var(--border);
+        color: inherit;
+      }
+      input::placeholder {
+        font-weight: bold;
+        opacity: 0.5;
+        color: red;
+      }
+    }
+    button {
+      cursor: pointer;
+      background-color: var(--primaryColor);
+      border: 1px solid var(--primaryColor);
+      color: var(--textColor);
+      font-weight: bold;
+      outline: none;
+      border-radius: var(--size1);
+    }
+    h2 {
+      font-size: 22px;
+      border-bottom: var(--border);
+      padding-bottom: var(--size1);
+    }
+    ul {
+      padding: 10px;
+      li {
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        border: var(--border);
+        padding: var(--size1) var(--size4);
+        border-radius: var(--size1);
+        margin-bottom: var(--size2);
+        span {
+          cursor: pointer;
+        }
+        .done {
+          text-decoration: line-through;
+        }
+        button {
+          font-size: var(--size2);
+          padding: var(--size1);
+        }
+      }
+    }
+    h4 {
+      text-align: center;
+      opacity: 0.5;
+      margin: 0;
+    }
+  }
 }
 </style>
